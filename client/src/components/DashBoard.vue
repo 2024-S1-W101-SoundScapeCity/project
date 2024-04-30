1bdea436312298eb995a4559432434ec61954aa9
fujimite <fujimite@users.noreply.github.com>
2024-04-30 12:23:13 +1200

    fix missing end tag

--- client/src/components/DashBoard.vue
+++ client/src/components/DashBoard.vue
@@ -2,13 +2,8 @@
   <div class="layout">
     <div id="menu">
       <div id="placeholder">logo</div>
-      <button
-        v-for="tab in tabs"
-        v-bind:key="tab.name"
-        v-bind:class="['tab-button', { active: currentTab === tab.name }]"
-        v-on:click="currentTab = tab.name"
-        @click="navigateToTab(tab.route)"
-      >
+      <button v-for="tab in tabs" v-bind:key="tab.name" v-bind:class="['tab-button', { active: currentTab === tab.name }]"
+        v-on:click="currentTab = tab.name" @click="navigateToTab(tab.route)">
         {{ tab.name }}
       </button>
       <component v-bind:is="currentTabComponent" class="tab-button"></component>
@@ -17,19 +12,13 @@
       <div id="tab-content">
         <router-view></router-view>
       </div>
-        <div id="content" class="map-container">
-        <iframe
-          class="map-iframe"
-          width="1080"
-          height="720"
-          frameborder="0"
-          scrolling="no"
-          allowfullscreen
-          src="https://arcg.is/1veiH00"
-        ></iframe>
-    </div>
-    <div id="footer">
-      <p>footer text</p>
+      <div id="content" class="map-container">
+        <iframe class="map-iframe" width="1080" height="720" frameborder="0" scrolling="no" allowfullscreen
+          src="https://arcg.is/1veiH00"></iframe>
+      </div>
+      <div id="footer">
+        <p>footer text</p>
+      </div>
     </div>
   </div>
 </template>
@@ -71,6 +60,7 @@ export default {
   margin-bottom: -1px;
   margin-right: -1px;
 }
+
 #menu button.active {
   padding: 6px 10px;
   border-top-left-radius: 3px;
@@ -80,6 +70,7 @@ export default {
   margin-bottom: -1px;
   margin-right: -1px;
 }
+
 .dashboard-container {
   display: flex;
   flex-direction: row;
@@ -88,22 +79,27 @@ export default {
   border-collapse: collapse;
   margin: 5px;
 }
+
 .layout {
   display: flex;
   flex-direction: column;
 }
+
 #menu {
   float: left;
   text-align: left;
 }
+
 #menu img {
   size: 10%;
 }
+
 #menu #placeholder {
   background-color: grey;
   width: 18vw;
   height: 6vw;
 }
+
 .dashboard-container #content {
   width: 80ch;
   height: 100vh;
@@ -111,27 +107,36 @@ export default {
   background-color: #4f4f4f;
   flex: initial;
 }
+
 .dashboard-container #tab-content {
   border: 1px solid #000000;
   flex: auto;
 }
+
 #footer {
   text-decoration: none;
   text-align: center;
   float: bottom;
 }
+
 #footer p {
   font-size: 70%;
 }
+
 #footer p span {
   font-size: 100%;
 }
+
 .map-container {
   position: relative;
-  width: 80ch; /* Same width as #content */
-  height: 100vh; /* Same height as #content */
+  width: 80ch;
+  /* Same width as #content */
+  height: 100vh;
+  /* Same height as #content */
 }
-.map-iframe {  /*to make the iframe fit the entire container*/
+
+.map-iframe {
+  /*to make the iframe fit the entire container*/
   position: absolute;
   top: 0;
   left: 0;
