c1a68ab7b5b3a81a440be5592d9d7d7b75625c7f
nomiDwigg <naomiwiggins@me.com>
2024-05-05 22:07:51 +1200

    setup headers for server files

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
+const history = require('connect-history-api-fallback')
 
 // create express server
 const app = express()
@@ -9,8 +11,39 @@ app.use(bodyparser.json())
 // cors
 app.use(cors())
 
-// define route in express
-app.get('/', (req, res) => {
-  res.send('hello world!')
+// serve static files from vue production folder client/dist
+const distPath = path.join('..', 'client', 'dist')
+const staticFileMiddleware = express.static(distPath)
+app.use(staticFileMiddleware)
+
+// history middleware
+app.use(history({ disableDotRule: true, verbose: true }))
+// app.use(customHist())
+
+// define api route to vue components
+// app.get('/', (req, res) => {
+//   res.json({ message: 'successful connection to the server!' })
+// })
+
+// set secure content policy for all header files
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy-Report-Only',
+    "default-src 'self'; script-src 'self' 'unsafe-inline' http://localhost:8080 http://localhost:8098; connect-src 'self' ws://localhost:8080 ws://localhost:8098;"
+  )
+  next()
 })
-app.listen(process.env.PORT || 8081)
+
+// define 404 route in express
+app.get('*', (req, res) => {
+  res.setHeader(
+    'Content-Security-Policy-Report-Only',
+    "default-src 'self'; script-src 'self' 'unsafe-inline' http://localhost:8080 http://localhost:8098; connect-src 'self' ws://localhost:8080 ws://localhost:8098;"
+  )
+  res.send(path.join(distPath, 'index.html'))
+})
+// define and listen on port
+const port = process.env.PORT || 8081
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
