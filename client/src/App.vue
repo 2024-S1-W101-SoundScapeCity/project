94e7b209ad2ea03380f017728b5598ecda91c4b6
fujimite <fujimite@users.noreply.github.com>
2024-06-03 22:28:06 +1200

    tidy unused features

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,4 @@
 <!-- App.vue -->
 <template>
-  <div id="app">
     <router-view></router-view>
-  </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      query: ''
-    };
-  },
-  methods: {
-    search() {
-      this.$emit('search', this.query);
-    }
-  }
-}
-</script>
